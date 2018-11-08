3752b57d73563ae0878c9d5e0f1536e170fa2fef
uoon-dev <uoon.dev@gmail.com>
2018-11-08 19:44:31 +0900

    apply new font

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -6,7 +6,7 @@ import Option from '../Option/Option';
 const options = (props) => {
   return (
     <div className={classes.Options}>
-      <h3>옵션</h3>
+      <h2 className={classes.SubTitle}>옵션</h2>
       <Option
         childOptions={props.type === 'fst' ? props.options.fst : props.options.sec}
         addNewOption={props.addNewOption}
